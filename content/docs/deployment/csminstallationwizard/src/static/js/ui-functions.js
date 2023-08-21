67eb097d1b02b8a71080b275a0eb24ea8b923a06
Chiman Jain <chimanjain15@gmail.com>
2023-08-21 21:49:32 +0530

    Updated UI for module support in powerscale

--- content/docs/deployment/csminstallationwizard/src/static/js/ui-functions.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/ui-functions.js
@@ -305,6 +305,9 @@ function displayModules(installationType, driverName, CONSTANTS_PARAM) {
 			$(".storage-capacity").show();
 			document.getElementById("driver-namespace").value = CONSTANTS_PARAM.POWERSCALE_NAMESPACE;
 			if (installationType === 'operator'){
+				$(".observability-operator").show();
+				$(".observability").hide();
+				$(".replication-operator").show();
 				$(".resiliency").hide();
 				$(".resiliency-operator").show();
 				$(".image-repository").hide();
