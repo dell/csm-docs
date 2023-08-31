d9b06ce4a36a487dfdd0e0f5a3d874356d13d232
Yamunadevi N Shanmugam <Yamunadevi.Shanmugam@dell.com>
2023-08-31 15:37:07 +0530

    replace yaml

--- content/docs/deployment/csminstallationwizard/src/static/js/generate-yaml.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/generate-yaml.js
@@ -229,9 +229,9 @@ function createYamlString(yamlTpl, yamlTplValues, driverParam, CONSTANTS_PARAM) 
 
 	yamlTpl = yamlTpl.replaceAll("$CERT_MANAGER_ENABLED", yamlTplValues.certManagerEnabled);
 	yamlTpl = yamlTpl.replaceAll("$OBSERVABILITY_CERT_MANAGER_ENABLED", !yamlTplValues.certManagerEnabled);
-	yamlTpl = yamlTpl.replace('      - storageArrayId: ""', '#      - storageArrayId: ""');
-	yamlTpl = yamlTpl.replace('        endpoint: ""', '#       endpoint: ""');	
-	yamlTpl = yamlTpl.replace('      - endpoint: ""', '#       endpoint: ""');
+	yamlTpl = yamlTpl.replaceAll('      - storageArrayId: ""', '#      - storageArrayId: ""');
+	yamlTpl = yamlTpl.replaceAll('        endpoint: ""', '#       endpoint: ""');	
+	yamlTpl = yamlTpl.replaceAll('      - endpoint: ""', '#       endpoint: ""');
 	const regex = /\$[a-zA-Z0-9_-]*/g;
 	yamlTpl = yamlTpl.replaceAll(regex, '""');
 
