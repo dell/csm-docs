f6f962de065cb31a3c8d03f88a9abfdce6ed0694
Chiman Jain <chimanjain15@gmail.com>
2023-08-31 17:56:25 +0530

    add max volume limit param in powerscale

--- content/docs/deployment/csminstallationwizard/src/static/js/ui-functions.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/ui-functions.js
@@ -366,6 +366,7 @@ function displayModules(installationType, driverName, CONSTANTS_PARAM) {
 			$(".fsGroupPolicy").show();
 			$(".vgsnapshot").hide();
 			$(".storage-capacity").show();
+			$(".max-volumes-per-node").show();
 			document.getElementById("driver-namespace").value = CONSTANTS_PARAM.POWERSCALE_NAMESPACE;
 			if (installationType === 'operator'){
 				$(".observability-operator").show();
