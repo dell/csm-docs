fcb63fae0cc4a5503f131458ac07ffc3676ed2a4
Khare <rajshree.khare@dell.com>
2023-09-01 18:06:28 +0530

    change reverted

--- content/docs/deployment/csminstallationwizard/src/static/js/generate-yaml.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/generate-yaml.js
@@ -240,7 +240,7 @@ function createYamlString(yamlTpl, yamlTplValues, driverParam, CONSTANTS_PARAM) 
 	yamlTpl = yamlTpl.replaceAll("$CERT_MANAGER_ENABLED", yamlTplValues.certManagerEnabled);
 	yamlTpl = yamlTpl.replaceAll("$OBSERVABILITY_CERT_MANAGER_ENABLED", !yamlTplValues.certManagerEnabled);
 	yamlTpl = yamlTpl.replaceAll('      - storageArrayId: ""', '#      - storageArrayId: ""');
-	yamlTpl = yamlTpl.replaceAll('        endpoint: ""', '#       endpoint: ""');
+	yamlTpl = yamlTpl.replaceAll('        endpoint: ""', '#       endpoint: ""');	
 	yamlTpl = yamlTpl.replaceAll('      - endpoint: ""', '#      - endpoint: ""');
 	const regex = /\$[a-zA-Z0-9_-]*/g;
 	yamlTpl = yamlTpl.replaceAll(regex, '""');
