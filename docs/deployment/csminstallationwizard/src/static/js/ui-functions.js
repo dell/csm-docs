047603f3c694c942a48c8293aa99797ac01b5c47
suryagupta4 <suryagupta4@users.noreply.github.com>
2024-04-04 13:53:33 +0000

    deploy: 9b596fbbb476eb23f0759d8bd1ceef7aab50224d

--- docs/deployment/csminstallationwizard/src/static/js/ui-functions.js
+++ docs/deployment/csminstallationwizard/src/static/js/ui-functions.js
@@ -472,11 +472,11 @@ function displayCommands(releaseNameValue, commandTitleValue, commandNoteValue, 
 		case "1.9.3":
 			helmChartVersion = CONSTANTS.CSM_HELM_V193;
 			break;
-		case "1.10.0":
-			helmChartVersion = CONSTANTS.CSM_HELM_V1100;
+		case "1.10.1":
+			helmChartVersion = CONSTANTS.CSM_HELM_V1101;
 			break;
 		default:
-			helmChartVersion = CONSTANTS.CSM_HELM_V1100;
+			helmChartVersion = CONSTANTS.CSM_HELM_V1101;
 			break;
 	}
 	$("#command-text-area").show();
