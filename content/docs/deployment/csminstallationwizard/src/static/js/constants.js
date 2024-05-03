225c3456458e581f6bfabc0fc5b72d9da9f718db
Kshitija Kakde <Kshitija_Kakde@Dell.com>
2024-05-03 15:35:42 +0530

    installation wizard changes

--- content/docs/deployment/csminstallationwizard/src/static/js/constants.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/constants.js
@@ -43,7 +43,7 @@ const CONSTANTS = {
 	CSM_HELM_V170: "1.0.0",
 	CSM_HELM_V180: "1.1.0",
 	CSM_HELM_V190: "1.2.1",
-	CSM_HELM_V1100: "1.3.0",
+	CSM_HELM_V1100: "1.3.1",
 	HELM_TAINTS: `
      - key: "$KEY"
        operator: "Exists"
