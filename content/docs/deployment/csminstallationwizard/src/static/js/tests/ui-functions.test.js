78f403dbed4f1e5d0592029f95c6b0ebfccb94b7
Rishabh Raj <rishabh.raj2@dell.com>
2024-09-23 10:53:58 +0000

    fixed review comments

--- content/docs/deployment/csminstallationwizard/src/static/js/tests/ui-functions.test.js
+++ content/docs/deployment/csminstallationwizard/src/static/js/tests/ui-functions.test.js
@@ -71,7 +71,7 @@ const CONSTANTS = {
 	CSM_HELM_V180: "1.1.0",
 	CSM_HELM_V193: "1.2.1",
 	CSM_HELM_V1102: "1.3.2",
-	CSM_HELM_V1111: "1.4.0",
+	CSM_HELM_V1111: "1.4.1",
 };
 
 describe("GIVEN onAuthorizationChange function", () => {
