3ca2c716ebc1c8e426eb77cf747afb451b17ad24
Yamunadevi N Shanmugam <yamunadevi.shanmugam@dell.com>
2026-01-14 16:08:40 +0530

    fixed installation wizard changes

--- content/docs/getting-started/installation/installationwizard/src/static/js/tests/generate-yaml.test.js
+++ content/docs/getting-started/installation/installationwizard/src/static/js/tests/generate-yaml.test.js
@@ -44,7 +44,7 @@ const CONSTANTS = {
 	OPERATOR: "operator",
 	CSM_HELM_V1140: "1.7.0",
 	CSM_HELM_V1150: "1.8.0",
-  CSM_HELM_V1160: "1.9.0",
+	CSM_HELM_V1160: "1.9.0",
 	HELM_TAINTS: `
     - key: "$KEY"
       operator: "Exists"
